5106a48f2e996e440aa42f92cfeb2ae6980e37ad
aruslantsev <andreiruslantsev@gmail.com>
2022-06-27 16:13:38 +0300

    update

--- nn/mnist/autoencoders.py
+++ nn/mnist/autoencoders.py
@@ -71,3 +71,52 @@ class DeepDecoder(torch.nn.Module):
         x = torch.sigmoid(x)
         x = x.reshape(-1, 28, 28)
         return x
+
+
+class ConvEncoder(torch.nn.Module):
+    def __init__(self):
+        super(ConvEncoder, self).__init__()
+        self.conv0 = torch.nn.Conv2d(
+            in_channels=1, out_channels=128, kernel_size=(7, 7), padding="same")
+        self.conv1 = torch.nn.Conv2d(
+            in_channels=128, out_channels=32, kernel_size=(3, 3), padding="same")
+        self.conv2 = torch.nn.Conv2d(
+            in_channels=32, out_channels=1, kernel_size=(7, 7), padding="same")
+        self.pooling = torch.nn.MaxPool2d((2, 2), padding="same")
+
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        x = self.conv0(x)
+        x = torch.nn.functional.relu(x)
+        x = self.pooling(x)
+        x = self.conv1(x)
+        x = torch.nn.functional.relu(x)
+        x = self.pooling(x)
+        x = self.conv2(x)
+        x = torch.nn.functional.relu(x)
+
+        return x
+
+
+class ConvDecoder(torch.nn.Module):
+    def __init__(self, num_features: int = 2):
+        super(ConvDecoder, self).__init__()
+        self.conv0 = torch.nn.Conv2d(
+            in_channels=1, out_channels=32, kernel_size=(7, 7), padding="same")
+        self.conv1 = torch.nn.Conv2d(
+            in_channels=32, out_channels=128, kernel_size=(3, 3), padding="same")
+        self.conv2 = torch.nn.Conv2d(
+            in_channels=128, out_channels=1, kernel_size=(7, 7), padding="same")
+        self.pooling = torch.nn.Upsample(scale_factor=(2, 2))
+
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        x = self.conv0(x)
+        x = torch.nn.functional.relu(x)
+        x = self.pooling(x)
+        x = self.conv1(x)
+        x = torch.nn.functional.relu(x)
+        x = self.pooling(x)
+        x = self.conv2(x)
+        x = torch.nn.functional.relu(x)
+        x = torch.sigmoid(x)
+        x = x.reshape(-1, 28, 28)
+        return x
