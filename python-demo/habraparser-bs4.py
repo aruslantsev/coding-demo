f5a492bd8d3943dc04112669b36d4eb18275a40a
Andrei Ruslantsev <andreiruslantsev@gmail.com>
2023-04-29 18:49:56 +0300

    update

--- python-demo/habraparser-bs4.py
+++ python-demo/habraparser-bs4.py
@@ -2,12 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get("https://habr.com/ru/post/1/")
-response.status_code
+_ = response.status_code
 
 soup = BeautifulSoup(response.text, "html.parser")
 
 author_uri = soup.find(class_="tm-user-info__username").get("href")
-author = a.get_text().strip()
+author = author_uri.get_text().strip()
 
 article_text = str(soup.find(id="post-content-body").get_text())
 title = soup.find(class_="tm-article-snippet__title tm-article-snippet__title_h1").get_text()
