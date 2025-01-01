f2d0eaa1fbae085fa69bcd22778cc8fae023a110
Andrei Ruslantsev <andreiruslantsev@gmail.com>
2025-01-01 10:08:25 +0200

    refactor

--- esp32/blink/main/blink_example_main.c
+++ esp32/blink/main/blink_example_main.c
@@ -17,15 +17,17 @@
 #define LED0_PIN     GPIO_NUM_18
 #define LED1_PIN     GPIO_NUM_19
 
+#define TICK_MICROSECONDS 1000000
 
-void led_tick(int mode)
+
+void led_switch(int mode, gpio_num_t pin0, gpio_num_t pin1)
 {
     if (mode) {
-        gpio_set_level(LED0_PIN, 1);
-        gpio_set_level(LED1_PIN, 0);
+        gpio_set_level(pin0, 1);
+        gpio_set_level(pin1, 0);
     } else {
-        gpio_set_level(LED0_PIN, 0);
-        gpio_set_level(LED1_PIN, 1);
+        gpio_set_level(pin0, 0);
+        gpio_set_level(pin1, 1);
     }
 }
 
@@ -45,27 +47,25 @@ void app_main(void)
     while(1) {
         if (gpio_get_level(BUTTON_PIN) != 0) {
             if (!is_pressed) {
-                printf("Button pressed, low level on button pin\n");
-                gpio_set_level(LED0_BTN_PIN, 1);
-                gpio_set_level(LED1_BTN_PIN, 0);
+                printf("Button pressed, high level on button pin\n");
+                led_switch(is_pressed, LED0_BTN_PIN, LED1_BTN_PIN);
                 is_pressed = 1;
             }
         } else {
             if (is_pressed) {
-                printf("Button released, high level on button pin\n");
-                gpio_set_level(LED0_BTN_PIN, 0);
-                gpio_set_level(LED1_BTN_PIN, 1);
+                printf("Button released, low level on button pin\n");
+                led_switch(is_pressed, LED0_BTN_PIN, LED1_BTN_PIN);
                 is_pressed = 0;
             }
         }
 
         vTaskDelay(1);
         uint64_t end = esp_timer_get_time();
-        if (end - start >= 1000000) {
-            printf("Blink: %d!\n", blink);
-            led_tick(blink);
+        if (end - start >= TICK_MICROSECONDS) {
+            printf("Blink: %d\n", blink);
+            led_switch(blink, LED0_PIN, LED1_PIN);
             blink = !blink;
-            start = end;
+            start += TICK_MICROSECONDS;
         }
 
     }
