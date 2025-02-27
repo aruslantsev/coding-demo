cb3db2d95bef9127847aae21b603f3260deccdb2
Andrei Ruslantsev <andreiruslantsev@gmail.com>
2025-02-28 00:25:01 +0200

    C examples

---
+++ c/struct_enum_demo.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+enum operation
+{
+    ADD = 1,
+    SUBTRACT = 2,
+    MULTIPLY = 4
+};
+
+struct BinaryOp{
+    int n1;
+    int n2;
+    enum operation op;
+};
+
+int main (void)
+{
+    struct BinaryOp mulOp = {.n1 = 6, .n2 = 8, .op = MULTIPLY};
+    printf("Operands: %d and %d. Operation: %d\n", mulOp.n1, mulOp.n2, mulOp.op);
+    // Operands: 6 and 8. Operation: 4
+    switch (mulOp.op) {
+        case ADD:
+            printf("%d\n", mulOp.n1 + mulOp.n2);
+            break;
+        case SUBTRACT:
+            printf("%d\n", mulOp.n1 - mulOp.n2);
+            break;
+        case MULTIPLY:
+            printf("%d\n", mulOp.n1 * mulOp.n2);
+            break;
+        default:
+            printf("Unknown op\n");
+            break;
+    }
+    return 0;
+}
---
+++ c/union_demo.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+// Union definition
+union A {
+    int i;
+    float f;
+    char s[20];
+};
+
+int main() {
+
+    union A a;
+
+    // Storing an integer
+    a.i = 10;
+    printf("data.i = %d\n", a.i);
+
+    // Storing a float
+    a.f = 220.5;
+    printf("data.f = %.2f\n", a.f);
+    printf("data.i = %d\n", a.i);  // Wrong value!
+
+    // Storing a string
+    strcpy(a.s, "GfG");
+    printf("data.s = %s\n", a.s);
+
+
+
+    return 0;
+}
