ed4634c70bfc07af111153d45c6f6a883c9a4e2d
Andrei Ruslantsev <andreiruslantsev@gmail.com>
2025-05-31 12:22:37 +0300

    Leetcode update

---
+++ leetcode/2894. Divisible and Non-divisible Sums Difference.c
@@ -0,0 +1,3 @@
+int differenceOfSums(int n, int m) {
+    return (n + 1) * n / 2 - (n / m + 1) * (n / m) * m;
+}
--- leetcode/2942. Find Words Containing Character.c
+++ leetcode/2942. Find Words Containing Character.c
@@ -1,3 +1,16 @@
+int* findWordsContaining(char** words, int wordsSize, char x, int* returnSize) {
+    int* res = (int*)malloc(wordsSize * sizeof(int));
+    *returnSize = 0;
+    for (int i = 0; i < wordsSize; i++) {
+        if (strchr(words[i], x) != NULL) {
+            res[(*returnSize)++] = i;
+        }
+    }
+    return res;
+}
+
+
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
