fb0014abaf7d8947498a52987e8e45af0f129d2d
Andrei Ruslantsev <andreiruslantsev@gmail.com>
2025-12-30 10:14:34 +0200

    Simpletron update

--- c/deitel/simpletron/write_test_programs.c
+++ c/deitel/simpletron/write_test_programs.c
@@ -13,7 +13,8 @@ void write_bindata(void) {
 }
 
 void write_data(void) {
-    FILE *file = fopen("add_numbers.spl", "w");
+    /* simpletron machine language */
+    FILE *file = fopen("add_numbers.sml", "w");
     char data[][WORD_BYTES * 2 + 1] = {"100a", "100b", "200a", "300b", "210c", "110c", "4300"};
     for (size_t i = 0; i < sizeof(data) / sizeof(char) / (WORD_BYTES * 2 + 1); i++)
         fprintf(file, "%s\n", data[i]);
